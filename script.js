be90b2f38c40fa205309687f47773d7dd08691a3
superlucky-star <supertrick901@gmail.com>
2026-07-16 18:22:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+const numbers = document.querySelectorAll(".number");
+
+numbers[0].innerHTML = "--";
+numbers[1].innerHTML = "--";
+numbers[2].innerHTML = "--";
+numbers[3].innerHTML = "--";
+
+});
