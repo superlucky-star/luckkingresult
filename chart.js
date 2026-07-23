75ec2fb8a9cd5c56807f5a01e7c55b0f79e042c3
superlucky-star <supertrick901@gmail.com>
2026-07-23 07:53:09 +0530

    Create chart.js

--- chart.js
+++ chart.js
@@ -1,56 +1,52 @@
 import { db } from "./firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+import { collection, getDocs, query, where } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-function changeMonth(){
-
-const month=document.getElementById("monthSelect").value;
-
-if(month=="july"){
-location.href="july2026.html";
-}
-
-if(month=="june"){
-location.href="june2026.html";
-}
-
-if(month=="may"){
-location.href="may2026.html";
-}
-
-if(month=="april"){
-location.href="april2026.html";
-}
-
-if(month=="march"){
-location.href="march2026.html";
-}
-
+function changeMonth() {
+    loadChart();
 }
 
 const chartBody = document.getElementById("chartBody");
+const year = "2026";
+const monthTitle = document.querySelector("#chartArea h3");
 
 async function loadChart() {
 
   chartBody.innerHTML = "";
+  
+  const month = document.getElementById("monthSelect").value;
 
-  const snapshot = await getDocs(collection(db, "chart"));
+monthTitle.innerText =
+  month.charAt(0).toUpperCase() + month.slice(1) + " " + year;
 
-  snapshot.forEach((doc) => {
+  const q = query(
+  collection(db, "chart"),
+  where("month", "==", month),
+  where("year", "==", year)
+);
 
-    const data = doc.data();
+const snapshot = await getDocs(q);
 
-    chartBody.innerHTML += `
-      <tr>
-        <td>${data.date}</td>
-        <td>${data.rdpr}</td>
-        <td>${data.blpr}</td>
-        <td>${data.gdpr}</td>
-        <td>${data.dnpr}</td>
-      </tr>
-    `;
+   const rows = [];
 
-  });
+snapshot.forEach((doc) => {
+    rows.push(doc.data());
+});
 
+rows.sort((a, b) => Number(a.date) - Number(b.date));
+
+rows.forEach((data) => {
+
+  chartBody.innerHTML += `
+    <tr>
+      <td>${data.date}</td>
+      <td>${data.rdpr}</td>
+      <td>${data.blpr}</td>
+      <td>${data.gdpr}</td>
+      <td>${data.dnpr}</td>
+    </tr>
+  `;
+
+});
 }
 
 loadChart();
